3de6115b6e0a25814c816d380b3b13b5c5627de1
amritg-create <76961652+amritg-create@users.noreply.github.com>
2024-07-01 23:00:40 -0400

    Update tryatpythonagain.py

--- tryatpythonagain.py
+++ tryatpythonagain.py
@@ -6,6 +6,7 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
+import sqlite3
 
 
 import streamlit as st
